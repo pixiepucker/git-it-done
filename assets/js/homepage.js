ed98c202b4004048dad05e68392c40b76dfccc6d
pixiepucker <autumnoffic@gmail.com>
2022-03-18 17:07:21 -0500

    added buttons to filter issues by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,5 @@
+var languageButtonsEl = document.querySelector("#language-buttons");
+
 var getUserRepos = function (user) {
     //format github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -81,7 +83,32 @@ var displayRepos = function (repos, searchTerm) {
     }
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+    
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+};
+
+var buttonClickHandler = function (event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+
+        //clear old content
+        repoContainerEl.textContent = '';
+    }
+};
+
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
